eb3d25103c7550fffaaa503c0e27ecf719b02ce2
asafMasa <asaf.masa@gmail.com>
2018-12-26 10:26:22 +0200

    first implemetation

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+
+declare const Cesium: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements AfterViewInit {
+  title = 'cesium-demo';
+
+
+
+  @ViewChild('cesiumContainer') cesiumContainer: ElementRef;
+
+  constructor() {
+
+
+  }
+
+  ngAfterViewInit(){
+    const cesiumBingLayer = new Cesium.BingMapsImageryProvider({
+      url: 'https://dev.virtualearth.net',
+      key: 'Ag9RlBTbfJQMhFG3fxO9fLAbYMO8d5sevTe-qtDsAg6MjTYYFMFfFFrF2SrPIZNq',
+      mapStyle: Cesium.BingMapsStyle.AERIAL_WITH_LABELS
+    });
+
+    new Cesium.Viewer(this.cesiumContainer.nativeElement, {
+      imageryProvider: cesiumBingLayer
+    });
+  }
+}
