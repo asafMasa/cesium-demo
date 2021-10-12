5fb506eca0ca1006dce13857f3596449f423a5d4
Asaf Masa <ASAFMAS@rafael.co.il>
2021-10-12 11:49:57 +0300

    working version with mapProxy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,30 +24,22 @@ export class AppComponent implements AfterViewInit {
     // });
 
     const mapProxyWmtsWGS84 =new Cesium.WebMapTileServiceImageryProvider({
-      url : 'http://mapproxy-dev-map-proxy-map-proxy-route-raster-dev.apps.v0h0bdx6.eastus.aroapp.io/wmts',
-      layer : 'MAS_6_ORT_247993-1.0',
-      style : 'newGrids',
-      format : 'image/jpeg',
-      tileMatrixSetID : 'default028mm',
-      // tileMatrixLabels : ['default028mm:0', 'default028mm:1', 'default028mm:2' ...],
-      maximumLevel: 19,
-      rectangle : Cesium.Rectangle.fromDegrees(34, 34, 36, 36)
-    })
-    // const mapProxyWmtsWGS84 = new Cesium.WebMapTileServiceImageryProvider({
-    //   url : '../wmts/bluemarble_il/{TileMatrixSet}/{TileMatrix}/{TileCol}/{TileRow}.png',
-    //   layer : 'bluemarble_il',
-    //   style : 'default',
-    //   format : 'image/png',
-    //   tileMatrixSetID : 'newGrids',
-    //   tilingScheme: new Cesium.GeographicTilingScheme()
-    // });
-
+      url : 'http://mapproxy-dev-map-proxy-map-proxy-route-raster-dev.apps.v0h0bdx6.eastus.aroapp.io/wmts/blue_marb_2/{TileMatrixSet}/{TileMatrix}/{TileCol}/{TileRow}.png',
+      layer : 'blue_marb_2',
+      style : 'default',
+      format : 'image/png',
+      tileMatrixSetID : 'newGrids',
+      tilingScheme: new Cesium.GeographicTilingScheme(),
+      rectangle : Cesium.Rectangle.fromDegrees(34, 30, 36, 34),
+    });
     const viewer = new Cesium.Viewer(this.cesiumContainer.nativeElement, {
-      imageryProvider: mapProxyWmtsWGS84
+      imageryProvider: mapProxyWmtsWGS84,
+      sceneMode : Cesium.SceneMode.COLUMBUS_VIEW,
+      mapProjection : new Cesium.GeographicProjection()
     });
     viewer.entities.add({
       rectangle : {
-        coordinates : Cesium.Rectangle.fromDegrees(34, 34, 36, 36),
+        coordinates : Cesium.Rectangle.fromDegrees(34, 30, 36, 34),
         material : Cesium.Color.GREEN.withAlpha(0.0),
         height : 0.0,
         outline : true,
