2f4258ad8125ae1f7cb94fd9c3da521d88b61581
Asaf Masa <ASAFMAS@rafael.co.il>
2021-10-12 10:54:02 +0300

    add map proxy support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,10 +19,20 @@ export class AppComponent implements AfterViewInit {
   }
 
   ngAfterViewInit() {
-    const cesiumOSMLayer = new Cesium.OpenStreetMapImageryProvider({
-      url : 'https://a.tile.openstreetmap.org/'
-    });
+    // const cesiumOSMLayer = new Cesium.OpenStreetMapImageryProvider({
+    //   url : 'https://a.tile.openstreetmap.org/'
+    // });
 
+    const mapProxyWmtsWGS84 =new Cesium.WebMapTileServiceImageryProvider({
+      url : 'http://mapproxy-dev-map-proxy-map-proxy-route-raster-dev.apps.v0h0bdx6.eastus.aroapp.io/wmts',
+      layer : 'MAS_6_ORT_247993-1.0',
+      style : 'newGrids',
+      format : 'image/jpeg',
+      tileMatrixSetID : 'default028mm',
+      // tileMatrixLabels : ['default028mm:0', 'default028mm:1', 'default028mm:2' ...],
+      maximumLevel: 19,
+      rectangle : Cesium.Rectangle.fromDegrees(34, 34, 36, 36)
+    })
     // const mapProxyWmtsWGS84 = new Cesium.WebMapTileServiceImageryProvider({
     //   url : '../wmts/bluemarble_il/{TileMatrixSet}/{TileMatrix}/{TileCol}/{TileRow}.png',
     //   layer : 'bluemarble_il',
@@ -32,8 +42,17 @@ export class AppComponent implements AfterViewInit {
     //   tilingScheme: new Cesium.GeographicTilingScheme()
     // });
 
-    new Cesium.Viewer(this.cesiumContainer.nativeElement, {
-      imageryProvider: cesiumOSMLayer
+    const viewer = new Cesium.Viewer(this.cesiumContainer.nativeElement, {
+      imageryProvider: mapProxyWmtsWGS84
+    });
+    viewer.entities.add({
+      rectangle : {
+        coordinates : Cesium.Rectangle.fromDegrees(34, 34, 36, 36),
+        material : Cesium.Color.GREEN.withAlpha(0.0),
+        height : 0.0,
+        outline : true,
+        outlineColor : Cesium.Color.WHITE
+      }
     });
   }
 }
